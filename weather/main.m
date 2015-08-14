5c461d87862d408812d8a5bf0ddf6d560b99ba8e
angryapes <angryapes@163.com>
2015-08-14 16:05:37 +0800

    Initial Commit

---
+++ weather/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weather
+//
+//  Created by XSH-iOS on 15/8/14.
+//  Copyright (c) 2015年 Junchao Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
